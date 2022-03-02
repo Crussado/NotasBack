4a58b35b02e42f99d65f841dd041b0b20cb076cc
Marianela <marianelaclara@gmail.com>
2022-03-01 22:53:45 -0300

    Se crean las APIs borrar, crear y devolver

--- nota/urls.py
+++ nota/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('<int:id_nota>/get/', views.get_nota, name='get'),
-    path('<int:id_nota>/delete/', views.delete_nota, name='delete')
+    path('<int:id_nota>/delete/', views.delete_nota, name='delete'),
+    path('dev_nota/', views.get_notas, name='devoler'),
+    path('create_nota/', views.create_nota, name='create')
 ]
